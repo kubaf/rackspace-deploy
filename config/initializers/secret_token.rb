a3720d61d6802d4de3b728afcbd1fe0dc7e847eb
Kuba Fietkiewicz <jfietkiewicz@attinteractive.com>
2013-11-02 14:01:04 -0700

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RackspaceDeploy::Application.config.secret_token = '85a5d5357b05416066e8fd3e0fdc5c3bec3c824a1968956571c1c390c521fb3f9f25ab4043621af9c7b3cac37b410af2198d70e5b5cbf4788452a53167deb7b1'
